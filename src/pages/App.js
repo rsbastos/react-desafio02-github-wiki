2c5a78b76ce3180b6cfb3589d00e5bb83003d869
rsbastos <renedossantosbastos@gmail.com>
2024-09-24 10:10:25 -0300

    :sparkles: Feat - Input component

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,13 @@
 import gitLogo from '../assets/github.png'
+import Input from '../components/Input';
+
 import { Container } from './styles'; 
 
 function App() {
   return (
     <Container>
       <img src={gitLogo} width={72} height={72} alt='github_logo'/>  
+      <Input />
     </Container>
   );
 }
--- src/pages/styles.js
+++ src/pages/styles.js
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100%;
     height: 90vh;
+
     margin: 20px 0 0 0;
-    display: flex;
-    alignItems: flex-start;
-    justify-content: center;
+
+    display: flex;    
+    flex-direction: column;
+    align-Items: center;
+    justify-content: flex-start;
 `
